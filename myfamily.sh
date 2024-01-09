e3ea618b3192adaccf8be5513f776a0854b09dce
Ayoub El Haddad <aelhadda@noreply.learn.zone01oujda.ma>
2024-01-09 13:50:28 +0000

    Update 'myfamily.sh'

--- myfamily.sh
+++ myfamily.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -s https://learn.zone01oujda.ma/assets/superhero/all.json | jq -r '.[] | select(.id == '$HERO_ID') | .connections.relatives'
+curl -s https://learn.zone01oujda.ma/assets/superhero/all.json | jq  '.[] | select(.id == '$HERO_ID') | .connections.relatives' | tr -d "\""
