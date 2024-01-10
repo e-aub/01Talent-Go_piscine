5e233fc150fed1a52b4bcdafc6ed746b2aa0f037
Christopher  Fremond <dev@01-edu.org>
2024-01-10 11:41:11 +0000

    Update 'printcomb.go'

--- printcomb.go
+++ printcomb.go
@@ -11,18 +11,15 @@ func PrintComb() {
 						z01.PrintRune(a)
 						z01.PrintRune(b)
 						z01.PrintRune(c)
-						if a != '7' || b != '8' || c != '9'{
-						z01.PrintRune(',')
-						z01.PrintRune(' ')
+						if a != '7' || b != '8' || c != '9' {
+							z01.PrintRune(',')
+							z01.PrintRune(' ')
+						}
 					}
 				}
 			}
 		}
 	}
-}
-                        z01.PrintRune('\n')
+	z01.PrintRune('\n')
 }
 
-func main() {
-	PrintComb()
-}
