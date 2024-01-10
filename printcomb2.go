01c3b1b33826d45761de8e8bdf8f642077f7b575
Ayoub El Haddad <aelhadda@noreply.learn.zone01oujda.ma>
2024-01-10 12:01:39 +0000

    Update 'printcomb2.go'

--- printcomb2.go
+++ printcomb2.go
@@ -2,16 +2,17 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func printcomb2() {
-	for a := '0'; a <= '9'; a++ {
-		for b := '1'; b <= '8'; b++ {
-			z01.PrintRune(a)
-			z01.PrintRune(b)
-			if a != '9' || b != '8' {
-				z01.PrintRune(',')
-				z01.PrintRune(' ')
-			}
-		}
-	}
-	z01.PrintRune('\n')
+func PrintComb2() {
+    for a := '0'; a <= '9'; a++ {
+        for b := '1'; b <= '8'; b++ {
+            z01.PrintRune(a)
+            z01.PrintRune(b)
+            
+            if a != '9' || b != '8' {
+                z01.PrintRune(',')
+                z01.PrintRune(' ')
+            }
+        }
+    }
+    z01.PrintRune('\n')
 }
