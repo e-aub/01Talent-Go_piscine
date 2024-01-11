969f51612361cdc02f275419e34cef8e79ab08fc
Christopher  Fremond <dev@01-edu.org>
2024-01-11 19:12:20 +0000

    Update 'atoi.go'

---
+++ atoi.go
@@ -0,0 +1,25 @@
+package piscine
+
+func Atoi(s string) int {
+	char := []rune(s)
+	res := 0
+	sign := 1
+
+	if char[0] == '-' {
+		char = char[1:]
+		sign = -1
+	}
+
+	if char[0] == '+' {
+		char = char[1:]
+	}
+	len := len(char)
+	for i := 0; i < len; i++ {
+		if char[i] >= '0' && char[i] <= '9' {
+			res = res*10 + int(char[i]-'0')
+		} else {
+			return 0
+		}
+	}
+	return res * sign
+}
