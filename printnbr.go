4509bcd12c425021d3ed5939ec3982bb9074c6a4
aelhadda <eaub@proton.me>
2024-01-11 07:46:48 +0000

    Good morning

---
+++ printnbr.go
@@ -0,0 +1,28 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func PrintNbr(n int) {
+	MinVal := -9223372036854775808
+	if n == MinVal {
+		PrintNbr(-922337203685477580)
+		PrintNbr(8)
+		return
+	}
+	if n < 0 {
+		z01.PrintRune('-')
+		PrintNbr(-n)
+	} else if n >= 0 && n < 10 {
+		z01.PrintRune(rune(n + 48))
+	} else if n >= 10 {
+		PrintNbr(n / 10)
+		PrintNbr(n % 10)
+	}
+}
+
+func main() {
+	PrintNbr(-9223372036854775808)
+	PrintNbr(0)
+	PrintNbr(123)
+	z01.PrintRune('\n')
+}
