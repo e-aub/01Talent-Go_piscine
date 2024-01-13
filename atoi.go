55f9763622cea0b046b08f809b57c58fc334ee86
Ayoub El Haddad <aelhadda@noreply.learn.zone01oujda.ma>
2024-01-13 09:42:52 +0000

    Upload files to ''

---
+++ atoi.go
@@ -0,0 +1,28 @@
+package piscine
+
+func Atoi(s string) int {
+	if s == "" || s == "-" || s == "+" {
+		return 0
+	}
+	char := []rune(s)
+	res := 0
+	sign := 1
+
+	if char[0] == '-' {
+		char = char[1:]
+		sign = -1
+	}else if char[0] == '+' {
+		char = char[1:]
+	}
+
+	len := len(char)
+	for i := 0; i < len; i++ {
+		if char[i] >= '0' && char[i] <= '9' {
+			res = res*10 + int(char[i]-'0')
+
+		} else {
+			return 0
+		}
+	}
+	return res * sign
+}
