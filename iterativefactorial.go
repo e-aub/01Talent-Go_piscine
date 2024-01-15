8164411e8d4c39f79a4c90c5ab6f7111ec8752e7
aelhadda <eaub@proton.me>
2024-01-15 10:51:03 +0000

    31

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,15 +1,12 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	if nb < 0 {
+	if nb < 0 || nb > 31 {
 		return 0
 	} else {
 		fact := 1
 		for i := 1; i <= nb; i++ {
 			fact = fact * i
-			if fact >= 9223372036854775807 {
-				return 0
-			}
 		}
 		return fact
 	}
