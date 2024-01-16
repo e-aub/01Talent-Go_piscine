b6a515773fbedb53fdaa5877fee98fe052837df6
Ayoub El Haddad <aelhadda@noreply.learn.zone01oujda.ma>
2024-01-16 11:06:42 +0000

    Update 'findnextprime.go'

--- findnextprime.go
+++ findnextprime.go
@@ -1,7 +1,6 @@
 package piscine
 
 func Prime(nb int) bool {
-	var res bool
 	if nb < 2 {
 		return false
 	} else if nb == 2 || nb == 3 {
@@ -9,12 +8,10 @@ func Prime(nb int) bool {
 	}
 	for i := 2; i <= nb/2; i++ {
 		if nb%i == 0 {
-			res = false
-		} else {
-			res = true
+			return false
 		}
 	}
-	return res
+	return true
 }
 
 func FindNextPrime(nb int) int {
