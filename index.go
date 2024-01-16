9da610337832c65583861c45ac71d2a26374e4fe
aelhadda <eaub@proton.me>
2024-01-16 18:07:39 +0000

    alpha

--- index.go
+++ index.go
@@ -1,6 +1,9 @@
 package piscine
 
 func Index(s string, toFind string) int {
+	if s == "" || toFind == "" {
+		return -1
+	}
 	toFindRune := []rune(toFind)
 	for index, letter := range s {
 		if letter == toFindRune[0] {
