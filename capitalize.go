bf404f5535273dea36f10a53f72bd4ee11fbce6c
aelhadda <eaub@proton.me>
2024-01-17 20:06:33 +0000

    dd

---
+++ capitalize.go
@@ -0,0 +1,21 @@
+package piscine
+
+func Capitalize(s string) string {
+	sRune := []rune(s)
+	for index, char := range sRune {
+		if char >= 'A' && char <= 'Z' {
+			sRune[index] = sRune[index] + 32
+		} else {
+			continue
+		}
+	}
+
+	for index, letter := range sRune {
+		if (letter >= 'a' && letter <= 'z') && index == 0 {
+			sRune[index] = sRune[index] - 32
+		} else if (letter >= 'a' && letter <= 'z') && (sRune[index-1] <= 47 && sRune[index-1] >= 32) {
+			sRune[index] = sRune[index] - 32
+		}
+	}
+	return string(sRune)
+}
