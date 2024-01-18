ad136aba5a169e40b1e2b0262ef0ab0a3b5a5591
Ayoub El Haddad <aelhadda@noreply.learn.zone01oujda.ma>
2024-01-18 10:58:35 +0000

    Update 'capitalize.go'

--- capitalize.go
+++ capitalize.go
@@ -13,7 +13,7 @@ func Capitalize(s string) string {
 	for index, letter := range sRune {
 		if (letter >= 'a' && letter <= 'z') && index == 0 {
 			sRune[index] = sRune[index] - 32
-		} else if (letter >= 'a' && letter <= 'z') && (sRune[index-1] <= 44 && sRune[index-1] >= 32) {
+		} else if (letter >= 'a' && letter <= 'z') && (sRune[index-1] <= 44 && sRune[index-1] >= 32) || (sRune[index-1] <= 63 && sRune[index-1] >= 58) {
 			sRune[index] = sRune[index] - 32
 		}
 	}
