ea4fbcb7be770d86a78e0e8ffe91e3bbe8e83857
aelhadda <eaub@proton.me>
2024-01-18 16:35:35 +0000

    finally

--- capitalize.go
+++ capitalize.go
@@ -2,22 +2,19 @@ package piscine
 
 func Capitalize(s string) string {
 	sRune := []rune(s)
-	if s == "" {
-		return s
-	}
-	for index, char := range sRune {
-		if char >= 'A' && char <= 'Z' {
-			sRune[index] = sRune[index] + 32
-		} else {
-			continue
+	for index, letter := range sRune {
+		if letter >= 'A' && letter <= 'Z' {
+			sRune[index] += 32
 		}
 	}
-
-	for index, letter := range sRune {
-		if (letter >= 'a' && letter <= 'z') && index == 0 {
-			sRune[index] = sRune[index] - 32
-		} else if (letter >= 'a' && letter <= 'z') && (sRune[index-1] <= 44 && sRune[index-1] >= 32) || (sRune[index-1] <= 63 && sRune[index-1] >= 58) {
-			sRune[index] = sRune[index] - 32
+	if sRune[0] >= 'a' && sRune[0] <= 'z' {
+		sRune[0] -= 32
+	}
+	for i := 0; i < len(sRune)-1; i++ {
+		if !(sRune[i] >= 'a' && sRune[i] <= 'z' || sRune[i] >= '0' && sRune[i] <= '9' || sRune[i] >= 'A' && sRune[i] <= 'Z') {
+			if sRune[i+1] >= 'a' && sRune[i+1] <= 'z' {
+				sRune[i+1] -= 32
+			}
 		}
 	}
 	return string(sRune)
