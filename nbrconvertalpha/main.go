290572c1fb246593e2315d78cbba9ec05144290d
aelhadda <eaub@proton.me>
2024-01-19 09:14:54 +0000

    nbrco

---
+++ nbrconvertalpha/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	// args variable
+	args := os.Args
+	// Checking for upper flag
+	i := 1
+
+	if args[1] == "--upper" {
+		i = 2
+	}
+	// atoi
+	var letter rune
+	for ; i < len(args); i++ {
+		argsint := 0
+		for _, let := range args[i] {
+			if let >= '0' && let <= '9' {
+				argsint = argsint*10 + int(rune(let)-'0')
+			}
+		}
+		if argsint <= 26 && argsint >= 1 {
+			if args[1] == "--upper" {
+				letter = rune(argsint + 64)
+			} else {
+				letter = rune(argsint + 96)
+			}
+			z01.PrintRune(letter)
+		} else {
+			z01.PrintRune(' ')
+		}
+
+	}
+	z01.PrintRune('\n')
+}
