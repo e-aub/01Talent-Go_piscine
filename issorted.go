b8d7a4887efb53ecd4c360a5ec6c4b9e31de91be
aelhadda <eaub@proton.me>
2024-01-23 20:44:47 +0100

    cmm

--- issorted.go
+++ issorted.go
@@ -4,7 +4,7 @@ func IsSorted(f func(a, b int) int, a []int) bool {
 	ascending := true
 	descending := true
 	for i := 0; i < len(a)-1; i++ {
-		if f(a[i], a[i+1]) == -1 {
+		if f(a[i], a[i+1]) < 0 {
 			ascending = false
 		} else if f(a[i], a[i+1]) == 0 {
 			continue
