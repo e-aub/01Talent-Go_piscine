906a24f2e82ef8253ab92aa6cc811ba9a757c8a6
aelhadda <eaub@proton.me>
2024-01-23 20:08:29 +0100

    countiff

--- issorted.go
+++ issorted.go
@@ -12,15 +12,3 @@ func IsSorted(f func(a, b int) int, a []int) bool {
 	}
 	return result
 }
-
-func Sort2(a, b int) int {
-	var res int
-	if a > b {
-		res = 1
-	} else if a == b {
-		res = 0
-	} else {
-		res = -1
-	}
-	return res
-}
