114dfd4201f929ffb11485f890cf3f9d0694cf25
aelhadda <eaub@proton.me>
2024-01-23 17:51:55 +0100

    countif

---
+++ countif.go
@@ -0,0 +1,13 @@
+package piscine
+
+func CountIf(f func(string) bool, tab []string) int {
+	var count int
+	for _, str := range tab {
+		if f(str) {
+			count++
+		} else {
+			continue
+		}
+	}
+	return count
+}
