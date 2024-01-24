d14102e8a1ec1bae78715cae94a65de4ffa972ab
aelhadda <eaub@proton.me>
2024-01-24 13:51:48 +0100

    unma

--- unmatch.go
+++ unmatch.go
@@ -1,25 +1,16 @@
 package piscine
 
 func Unmatch(a []int) int {
-	var res int
+	var count int
 	for i := 0; i < len(a); i++ {
-		for j := i; j < len(a); j++ {
-			if a[i] > a[j] {
-				a[i], a[j] = a[j], a[i]
-			} else {
-				continue
+		for j := 0; j < len(a); j++ {
+			if a[i] == a[j] {
+				count++
 			}
 		}
-	}
-
-	for _, nb := range a {
-		for _, nb2 := range a {
-			if nb == nb2 {
-				continue
-			} else {
-				res = nb
-			}
+		if count%2 != 0 {
+			return a[i]
 		}
 	}
-	return res
+	return -1
 }
