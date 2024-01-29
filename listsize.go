58eff360151fd300ce01a6274e4fe1fd4b241b84
eaub <eaub@proton.me>
2024-01-29 19:09:29 +0100

    add struct

--- listsize.go
+++ listsize.go
@@ -1,5 +1,14 @@
 package piscine
 
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
 func ListSize(l *List) int {
 	var count int
 	current := l.Head
